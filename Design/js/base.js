9edbcc29bd268262a19b0ad07ea12edf4561b7cd
Andy Fang <yzmtf2008@gmail.com>
2014-03-08 02:28:50 +0800

    New changes

--- Design/js/base.js
+++ Design/js/base.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
     $.fn.fullpage({
 				'verticalCentered': true,
 				'css3': true,
-				'slidesColor': ['rgb(153,225,108)', 'rgb(73,181,235)', '#fff', '#fff'],
+				'slidesColor': ['rgb(153,225,108)', 'rgb(73,181,235)', '#fe767a', '#fff'],
 				'navigation': true,
 				'navigationPosition': 'right',
 				'navigationTooltips': ['Welcome', 'Next', 'Amazing', 'Simple'],
-				
+                slidesNavigation: true,
+				slidesNavPosition: 'bottom',
 			});
 });
