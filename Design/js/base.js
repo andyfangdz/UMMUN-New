39eef12f354049d93183d51ff7419fe0d8bbc210
Andy Fang <yzmtf2008@gmail.com>
2014-03-09 01:13:47 +0800

    Big change

--- Design/js/base.js
+++ Design/js/base.js
@@ -2,11 +2,19 @@ $(document).ready(function() {
     $.fn.fullpage({
 				'verticalCentered': true,
 				'css3': true,
-				'slidesColor': ['rgb(153,225,108)', 'rgb(73,181,235)', '#fe767a', '#fff'],
+				'slidesColor': ['#a1e877', '#46abe0', '#fe767a', '#fff','#f5f4f7', '#faf189'],
 				'navigation': true,
 				'navigationPosition': 'right',
-				'navigationTooltips': ['Welcome', 'Next', 'Amazing', 'Simple'],
+				'navigationTooltips': ['Welcome', 'Next', 'Amazing', 'Simple', 'Join', 'Creators'],
                 slidesNavigation: true,
 				slidesNavPosition: 'bottom',
 			});
+    var defaultVid = 'XNjExMTE0NzMy';
+			getYoukuMp4Url(
+				defaultVid, 
+				function(mp4Url)
+				{
+					$('#youkuvideo').attr('src', mp4Url);
+				}
+			);
 });
